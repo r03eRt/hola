9d732920ead1bcc05cdddbd8e65c6e3d01f3325f
r03eRt <morgadoluengo@gmail.com>
2014-06-30 02:26:39 +0200

    prueba2

--- hola/src/com/example/hola/MainActivity.java
+++ hola/src/com/example/hola/MainActivity.java
@@ -56,7 +56,8 @@ public class MainActivity extends ActionBarActivity {
 		public View onCreateView(LayoutInflater inflater, ViewGroup container,
 				Bundle savedInstanceState) {
 			View rootView = inflater.inflate(R.layout.fragment_main, container,
-					false);
+					false);////
+			////
 			return rootView;
 		}
 	}
